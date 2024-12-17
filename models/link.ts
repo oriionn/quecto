ab6a17949d5e12652c0c7508d25cac858af74672
Orion <contact@oriondev.fr>
2024-12-17 15:39:29 +0100

    Add db handler

---
+++ models/link.ts
@@ -0,0 +1,6 @@
+export interface Link {
+    link: string;
+    short_code: string;
+    expiration: number;
+    password?: string;
+}
--- utils/db.ts
+++ utils/db.ts
@@ -1,14 +1,46 @@
 import { Database } from "bun:sqlite";
+import { Link } from "../models/link";
 
 class SQLiteDataHandler {
     private db: Database;
 
     constructor() {
-        this.db = new Database("data/db.sqlite")
+        this.db = new Database("data/db.sqlite", {strict: true})
 
         this.db
         .query(`CREATE TABLE IF NOT EXISTS links (short_code TEXT PRIMARY KEY, link TEXT NOT NULL, expiration INT NOT NULL, password TEXT)`)
         .run()
+    }
+
+    get(short_code: string) {
+        let query = this.db.prepare("SELECT * FROM links WHERE short_code = $short_code");
+        let results: Link = query.get({short_code})
+        return results;
+    }
+
+    getAll() {
+        let query = this.db.prepare("SELECT * FROM links")
+        let results: Link[] = query.all()
+        
+        let sanitizedResult: Link[] = []
+        results.forEach(link => {
+            delete link.password;
+            sanitizedResult.push(link);
+        })
+
+        return results;
+    }
+
+    create(link: string, short_code: string, expiration: number, password?: string): Link | null {
+        let query = this.db.prepare("INSERT INTO links (short_code, link, expiration, password) VALUES ($short_code, $link, $expiration, $password);");
+        try {
+            query.run({ short_code, link, expiration, password });
+            return {
+                short_code, link, expiration
+            }
+        } catch (error) {            
+            return null;
+        }
     }
 }
 
