388a1b2c1b1341078e922fba206c73a728331285
oriionn <contact@oriondev.fr>
2024-12-17 20:14:12 +0100

    feat: get password, db handler

--- utils/db.ts
+++ utils/db.ts
@@ -8,25 +8,24 @@ class SQLiteDataHandler {
         this.db = new Database("data/db.sqlite", {strict: true})
 
         this.db
-        .query(`CREATE TABLE IF NOT EXISTS links (short_code TEXT PRIMARY KEY, link TEXT NOT NULL, expiration INT NOT NULL, password TEXT)`)
+        .query(`CREATE TABLE IF NOT EXISTS links (short_code TEXT PRIMARY KEY, link TEXT NOT NULL, expiration INT NOT NULL, password TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`)
         .run()
     }
 
-    get(short_code: string) {
-        let query = this.db.prepare("SELECT * FROM links WHERE short_code = $short_code");
+    get(short_code: string, password?: boolean) {
+        if (password) {
+            let query = this.db.prepare("SELECT * FROM links WHERE short_code = $short_code");
+            let results = query.get({short_code})
+            return results;
+        }
+        let query = this.db.prepare("SELECT short_code, link, expiration, created_at FROM links WHERE short_code = $short_code");
         let results: Link = query.get({short_code})
         return results;
     }
 
     getAll() {
-        let query = this.db.prepare("SELECT * FROM links")
+        let query = this.db.prepare("SELECT short_code, link, expiration, created_at FROM links")
         let results: Link[] = query.all()
-        
-        let sanitizedResult: Link[] = []
-        results.forEach(link => {
-            delete link.password;
-            sanitizedResult.push(link);
-        })
 
         return results;
     }
