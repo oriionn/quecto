f2843c379d3c781bcbc81012040ced2103ab3954
oriionn <contact@oriondev.fr>
2025-01-13 22:35:46 +0100

    Patch Title Showing

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -1,20 +1,23 @@
-import {Component} from "solid-js";
+import {Component, Show} from "solid-js";
 import {Config} from "~/models/config";
 import {Meta, MetaProvider, Title} from "@solidjs/meta";
 
 const MetaC: Component<{ config: Config | undefined, title: string }> = (props) => {
+    console.log(props.config?.instance);
+
     return (
         <MetaProvider>
-            <Title>{props.config?.instance} - {props.title}</Title>
+            <Show when={props.config}>
+                <Title>{props.config?.instance} - {props.title}</Title>
+            </Show>
 
-            <Meta property="og:title" content={`${props.config?.instance} - ${props.title}`} />
+            <Meta property="og:title" content={props.title} />
             <Meta property="og:type" content="website" />
             <Meta property="og:description" content="A simple link shortener" />
-            <Meta property="og:site_name" content={props.config?.instance} />
             <Meta name="description" content="A simple link shortener" />
 
             <Meta name="twitter:card" content="summary" />
-            <Meta name="twitter:title" content={`${props.config?.instance} - ${props.title}`} />
+            <Meta name="twitter:title" content={props.title} />
             <Meta name="twitter:description" content="A simple link shortener" />
         </MetaProvider>
     )
